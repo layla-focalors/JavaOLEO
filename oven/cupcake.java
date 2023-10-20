db35eacda39b1d911b7fd748dc45d169a3661861
arisia <starlights2283@gmail.com>
2023-10-20 18:01:51 +0900

    add profile

---
+++ oven/cupcake.java
@@ -0,0 +1,17 @@
+package oven;
+// 1~5 삼각별 찍기
+import java.util.Scanner;
+
+public class cupcake {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input star triangle max value : ");
+        int max = sc.nextInt();
+        for(int i = 0; i < max; i ++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
