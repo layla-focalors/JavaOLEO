f5af6379fa51c63573c576c2814d9cbbb34e295e
arisia <starlights2283@gmail.com>
2023-10-20 16:44:55 +0900

    Add OREO

---
+++ package/string.java
@@ -0,0 +1,7 @@
+public class string{
+    public static void main(String[] args){
+        String sentecne = "A Polite conversaion about something much less important like traffic or weather";
+        // 문자열 리터럴 사용 > 공백 o, 여러 개의 문자를 저장할 수 있고, 내부적으로는 캐릭터 자료형으로 설계됨
+
+    }
+}
