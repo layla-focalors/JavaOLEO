4ed13004bd68a74349d9e54a89db11314475a5f7
arisia <starlights2283@gmail.com>
2023-10-23 15:57:41 +0900

    추가 : 뉴욕 치즈케이크

---
+++ 케이크/뉴욕치즈케이크.java
@@ -0,0 +1,16 @@
+package 케이크;
+
+public class 뉴욕치즈케이크 {
+    public static void main(String[] args){
+        String Sentence = "A polite conversation about something much less important like traffic or weather";
+
+        // 특정 인덱스의 문자 출력
+        System.out.println(Sentence.charAt(0));
+        
+        // 여러 줄 출력
+        System.out.println(Sentence.substring(2, 8));
+
+        // Sentence의 길이 출력
+        System.out.println(Sentence.length());
+    }
+}
